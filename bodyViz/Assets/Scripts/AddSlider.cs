d6aeefd6c0e036cb54959e4c39100db7525a4478
Amomorning <amomorning@gmail.com>
2019-03-22 12:11:05 +0800

    adding slider...

---
+++ bodyViz/Assets/Scripts/AddSlider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddSlider : MonoBehaviour
+{
+    public Slider sliderInstance;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void textUpdate(float value)
+    {
+        text.text = Mathf.RoundToInt(value * 100) + "cm";
+    }
+}
